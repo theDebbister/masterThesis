88151eef2e1f5ad991b839dd899c67346695a68a
theDebbister <deborahnoemie.jakobi@uzh.ch>
2021-08-31 11:42:30 +0200

    csv processing enabled

--- Code/PhoneticTextCreator.py
+++ Code/PhoneticTextCreator.py
@@ -1,20 +1,36 @@
 import re
-
 import pandas as pd
 from io import TextIOWrapper
+import csv
 
 
 class PhoneticTextCreator:
 
-    def __init__(self, wordlist: TextIOWrapper, text: TextIOWrapper):
-        self.wordlist = self._prepare_wordlist(wordlist)
-        self.text = self._preprocess_text(text)
+    def __init__(self, text: str, csv=False, wordlist=''):
+        self.csv = csv
+        self.percentage = []
+        self.preprocessed_text = []
+        self.phonetic_text = []
+        self.preprocessed_wordlist = []
 
-        self.percentage = 0
+        if self.csv:
+            self.csv_file = text
+            self._process_csv()
+        else:
+            self._create_phonetic_text(text, wordlist)
+
+
+
+    def _process_csv(self):
+        with open(self.csv_file, 'r', encoding='utf8') as file:
+            data_reader = csv.reader(file, delimiter=',')
+            for row in data_reader:
+                self._create_phonetic_text(row[0], row[1])
 
     @staticmethod
-    def _preprocess_text(text) -> [str]:
-        text = text.read()
+    def _preprocess_text(text: str) -> [str]:
+        with open(text, 'r', encoding='utf8') as text_file:
+            text = text_file.read()
         text = text.lower()
         text = re.sub(r'[\?!:;\.,\"\(\)]', "", text)
         text = re.sub(r'[\'\-]', " ", text)
@@ -23,7 +39,7 @@ class PhoneticTextCreator:
         return text
 
     @staticmethod
-    def _prepare_wordlist(wordlist: TextIOWrapper) -> dict:
+    def _prepare_wordlist(wordlist: str) -> dict:
         df_wordlist = pd.read_csv(wordlist, sep='\t', names=['word', 'pronunciation'])
 
         vocabulary = dict(zip(df_wordlist.word, df_wordlist.pronunciation))
@@ -33,15 +49,21 @@ class PhoneticTextCreator:
     def get_phonetic_text(self):
         return self.text
 
-    def create_phonetic_text(self):
+    def _create_phonetic_text(self, text, wordlist):
+        text = self._preprocess_text(text)
+        self.preprocessed_text.append(text)
+
+        wordlist = self._prepare_wordlist(wordlist)
+        self.preprocessed_wordlist.append(wordlist)
+
         phonetic_text = ""
 
         count_exist = 0
         count_not_exist = 0
 
-        for word in self.text:
-            if word.strip() in self.wordlist:
-                phonetic_text += re.sub(r' ', '', self.wordlist[word]) + " "
+        for word in text:
+            if word.strip() in wordlist:
+                phonetic_text += re.sub(r' ', '', wordlist[word]) + " "
                 count_exist += 1
             else:
                 splittable = False
@@ -50,17 +72,27 @@ class PhoneticTextCreator:
                 for n in range(len(word.strip()) - 3):
                     w1 = word[:n + 2]
                     w2 = word[n + 2:]
-                    if w1 in self.wordlist and w2 in self.wordlist:
-                        phonetic_text += self.wordlist[w1] + self.wordlist[w2] + " "
+                    if w1 in wordlist and w2 in wordlist:
+                        phonetic_text += wordlist[w1] + wordlist[w2] + " "
                         splittable = True
                         count_exist += 1
-                        print(w1, w2, self.wordlist[w1], self.wordlist[w2])
+                        print(w1, w2, wordlist[w1], wordlist[w2])
                         break
 
                 if not splittable:
                     phonetic_text += word.upper() + " "
                     count_not_exist += 1
 
-        self.percentage = round((count_exist / (count_not_exist + count_exist)) * 100, 2)
+        self.percentage.append(round((count_exist / (count_not_exist + count_exist)) * 100, 2))
+        self.phonetic_text.append(phonetic_text)
 
-        return phonetic_text, self.percentage
+    def create_csv(self):
+        d = {'preprocessed-text': [' '.join(text) for text in self.preprocessed_text],
+             'wordlist': self.preprocessed_wordlist,
+             'Phonetic-text': self.phonetic_text,
+             'Percentage of words in pron dict': self.percentage,
+             }
+
+        df = pd.DataFrame(d)
+
+        df.to_csv("example_output.csv", index=False)
--- Code/cli.py
+++ Code/cli.py
@@ -10,29 +10,47 @@ def get_parser() -> argparse.ArgumentParser:
         description="Replace full text with phonetic description using pronunciation dictionaries")
 
     pars.add_argument('text',
-                      help='text file with text in specific language',
-                      type=argparse.FileType('r', encoding='utf-8'),
+                      help='Text(s) that need to be converted into phonetic texts. '
+                           'Either txt file or csv containing paths.'
+                           'Csv must be first text path, then wordlist path.',
                       )
-    pars.add_argument('wordlist',
-                      help='tsv file that stores words and their pronunciationin IPA',
-                      type=argparse.FileType('r', encoding='utf-8'),
+    pars.add_argument('--wordlist', '-wl',
+                      help='tsv file that stores words and their pronunciation in IPA',
+                      required=False,
                       )
-    parser.add_argument('--directory', '-d',
-                        help='if it is an entire directory that needs to be precessed',
-                        action='store_const',
-                        const=True)
 
+    pars.add_argument('--csv',
+                      help='If it is a csv file containing paths to both text and wordlist',
+                      action='store_const',
+                      const=True,
+                      default=False)
 
     return pars
+
+
+def validate_args(arguments: argparse.Namespace) -> dict:
+    args_dict = {
+        arg: value for arg, value in vars(arguments).items()
+        if value is not None
+    }
+
+    # args csv and wl are mutually exclusive
+    if args_dict["csv"] == ("wordlist" in args_dict):
+        print("You can either combine text and wordlist or specify that it is an csv file that needs to be processed.")
+        exit(1)
+
+    return args_dict
 
 
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    print(args.text)
 
-    processor = PhoneticTextCreator(args.wordlist, args.text)
+    valid_args = validate_args(args)
 
-    print(processor.create_phonetic_text())
+    print(valid_args)
 
+    processor = PhoneticTextCreator(**valid_args)
+
+    processor.create_csv()
 
