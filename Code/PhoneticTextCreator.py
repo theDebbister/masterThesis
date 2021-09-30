4f3e4f1707cfa9ea0c81aa380df25e0e6bfdc0bb
theDebbister <deborahnoemie.jakobi@uzh.ch>
2021-09-30 17:44:30 +0200

    added WER CER table

--- Code/PhoneticTextCreator.py
+++ Code/PhoneticTextCreator.py
@@ -1,10 +1,40 @@
+import codecs
 import re
+import unicodedata
+
 import pandas as pd
 import csv
 import jiwer
 import Levenshtein as lev
+import hebrew_tokenizer as heb_tok
+from spacy.lang.zh import Chinese
+from spacy.lang.en import English
 
 
+# Applies to lines 31 - 44
+# MIT License
+#
+# Copyright (c) 2021 Deborah Jakobi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# https://github.com/jpuigcerver/xer --> source of lines 31 - 44
 def cer(hypothesis, reference):
     cer_s, cer_i, cer_d, cer_n = 0, 0, 0, 0
     # update CER statistics
@@ -44,7 +74,7 @@ class PhoneticTextCreator:
     def _process_csv(self) -> None:
         with open(self.csv_file, 'r', encoding='utf8') as file, \
                 open(self.output_name, 'a', encoding='utf8', newline='') as output, \
-            open(self.stats_name, 'a', encoding='utf8', newline='') as stats:
+                open(self.stats_name, 'a', encoding='utf8', newline='') as stats:
             data_reader = csv.reader(file, delimiter=';')
             data_writer = csv.writer(output, delimiter='\t')
             stats_writer = csv.writer(stats, delimiter=',')
@@ -52,8 +82,9 @@ class PhoneticTextCreator:
             all_langs = []
             num = 0
             for row in data_reader:
-                lang, path_org, path_wordlist, reference, type_ref, type_list = row[0], row[1], row[2], row[3], row[4], row[5]
-
+                lang, path_org, path_wordlist, reference, type_ref, type_list, num_w = row[0], row[1], row[2], \
+                                                                                       row[3], row[4], row[5], row[6]
+                print(lang)
                 with open(reference, 'r', encoding='utf8') as r:
                     ref_text = r.read()
 
@@ -73,7 +104,7 @@ class PhoneticTextCreator:
                                                                                                  lang)
 
                 wer = round(jiwer.wer(ref_text, phonetic_text), 2)
-                print(lang)
+
                 cer_val = round(cer(ref_text, phonetic_text), 2)
 
                 with open(new_file_name, 'w', encoding='utf8') as new_phonetic:
@@ -84,7 +115,7 @@ class PhoneticTextCreator:
 
                 data_writer.writerow(new_row)
 
-                new_stats_row = [lang, per_transcribed, wer, cer_val, type_ref, type_list]
+                new_stats_row = [lang, per_transcribed, wer, cer_val, type_ref, type_list, num_w]
 
                 stats_writer.writerow(new_stats_row)
 
@@ -92,7 +123,7 @@ class PhoneticTextCreator:
         header = ['lang-code (iso 639-3)', 'coverage', 'per-concat', 'per-unk', 'WER', 'CER', 'path-original',
                   'path-wordlist', 'path-phonetic-text', 'type-ref', 'type-list']
 
-        stats_header = ['lang-code (iso 639-3)', 'coverage', 'WER', 'CER', 'type-ref', 'type-list']
+        stats_header = ['Iso 639-3', 'Coverage', 'WER', 'CER', 'Type ref', 'Type list', 'Num words list']
 
         with open(self.output_name, 'w', newline='') as output:
             writer = csv.writer(output, delimiter='\t')
@@ -103,18 +134,35 @@ class PhoneticTextCreator:
             writer.writerow(stats_header)
 
     @staticmethod
-    def _preprocess_text(text: str) -> [str]:
-        try:
-            with open(text, 'r', encoding='utf8') as text_file:
-                text = text_file.read()
-            text = text.lower()
-            text = re.sub(r'[\?!:;\.,\"\(\)]', "", text)
-            text = re.sub(r'[\'\-。，]', " ", text)
-            text = text.split()
-        except UnicodeDecodeError:
-            print(text)
+    def _preprocess_text(path_original: str, lang: str) -> [str]:
+        """
+        preprocesses the original text: remove punctuation, tokenize and lower case
+        :param path_original:
+        :param lang:
+        :return:
+        """
+        tokens = []
 
-        return text
+        try:
+            with open(path_original, 'r', encoding='utf8') as text_file:
+                text = text_file.read()
+            # Language specific tokenizers
+            if lang == 'cmn':
+                nlp = Chinese()
+                tokenizer = nlp.tokenizer
+                for token in tokenizer(text):
+                    tokens.append(str(token))
+            else:
+                text = text.lower()
+                text = re.sub(r'[\?!:;\.,\"\(\)]', "", text)
+                text = re.sub(r'[\'\-。，]', " ", text)
+                tokens = text.split()
+
+        # in case that the fil encoding is wrong
+        except UnicodeDecodeError:
+            print(path_original)
+
+        return tokens
 
     @staticmethod
     def _prepare_wordlist(wordlist: str) -> dict:
@@ -127,12 +175,10 @@ class PhoneticTextCreator:
     def get_phonetic_text(self):
         pass
 
-    def _create_phonetic_text(self, text, wordlist, lang):
-        text = self._preprocess_text(text)
-        self.preprocessed_text.append(text)
+    def _create_phonetic_text(self, path_original, wordlist, lang):
+        text = self._preprocess_text(path_original, lang)
 
         wordlist = self._prepare_wordlist(wordlist)
-        self.preprocessed_wordlist.append(wordlist)
 
         phonetic_text = ""
 
