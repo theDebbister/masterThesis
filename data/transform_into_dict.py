69415fe005dabd006d82d84e8cf2d8055ddc5e24
theDebbister <deborahnoemie.jakobi@uzh.ch>
2021-10-20 18:14:53 +0200

    script to transform into dict

---
+++ data/transform_into_dict.py
@@ -0,0 +1,36 @@
+import sys
+import segments as seg
+
+
+def transform_into_dict(transcript:str, ortho: str, output):
+    with open(transcript, 'r', encoding='utf8') as trans, open(ortho, 'r', encoding='utf8') as ortho:
+        transcript_lines = trans.readlines()
+        ortho_lines = ortho.readlines()
+        
+    ortho_tokens = []
+    for line in ortho_lines:
+        ortho_tokens.extend(line.lower().split())
+        
+    trans_tokens = []
+    lang_profile = seg.Profile().from_textfile(transcript)
+    for line in transcript_lines:
+        tokenizer = seg.Tokenizer(lang_profile)
+        tokens = tokenizer(line)
+        trans_tokens.extend(tokens.split('#'))
+        
+    if len(trans_tokens) == len(ortho_tokens):
+        with open(output + '.dict', 'w', encoding='utf8') as output:
+            for t, o in zip(trans_tokens, ortho_tokens):
+                output.write(o.strip(' .') + '\t' + t.strip(' .') + '\n')
+    else:
+        print('COULD NOT CREATE DICT FOR', transcript.strip('.txt').strip("\theNorthWindAndTheSun\\").upper()) 
+            
+        
+        
+
+if __name__ == '__main__':
+    t = sys.argv[1]
+    o = sys.argv[2]
+    output = sys.argv[3]
+    
+    transform_into_dict(t, o, output)
--- jipa/README.md
+++
@@ -1,3 +0,0 @@
-# Journal of the IPA
-
-Northwind and Sun Illustrations dataset.
